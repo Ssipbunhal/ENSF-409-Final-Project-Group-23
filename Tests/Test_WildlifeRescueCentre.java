0dfbbd17fc25ab68ec0e57158785f6e7b4e16589
hamzahaydar <hamzahaydar@ucalgary.ca>
2023-03-23 16:53:15 -0600

    Wildlife Rescue and Staff Vet tests

---
+++ Tests/Test_WildlifeRescueCentre.java
@@ -0,0 +1,108 @@
+package Tests;
+
+import org.junit.Test;
+
+public class Test_WildlifeRescueCentre {
+    /*
+     * Test creating a new medical task
+     */
+    @Test
+    public void test_add_MedicalTask() {
+
+    }
+    /*
+     * Test adding a new feeding time
+     */
+    @Test
+    public void test_add_FeedingTime() {
+
+    }
+    /*
+     * Test adding a new volunteer
+
+     */
+    @Test
+    public void test_add_Volunteer() {
+
+    }
+    /*
+     * Test adding a new animal
+     */
+    @Test
+    public void test_add_Animal() {
+
+    }
+    /*
+     * Test getting medical tasks
+     */
+    @Test
+    public void test_get_MedicalTasks() {
+
+    }
+    /*
+     * Test getting feeding times
+     */
+    @Test
+    public void test_get_FeedingTimes() {
+
+    }
+    /*
+     * Test getting volunteers
+     */
+    @Test
+    public void test_get_Volunteers() {
+
+    }
+
+    /*
+     * Test getting Animals
+     */
+    @Test
+    public void test_get_Animals() {
+
+    }
+
+    /*
+     * Test adding a backup volunteer
+     *
+     */
+    @Test
+    public void test_add_BackupVolunteer() {
+
+    }
+    /*
+     * Test adding and getting multiple medical tasks for different animals
+     *
+     */
+    @Test
+    public void test_add_get_MedicalTasks() {
+
+    }
+    /*
+     * Test adding and getting multiple feeding times for different animals
+     *
+     */
+    @Test
+    public void test_add_getFeedingTimes() {
+
+    }
+
+    /*
+     * Test adding and getting multiple volunteers with different details
+     *
+     */
+    @Test
+    public void test_add_get_Volunteers() {
+
+    }
+
+    /*
+     * Test adding and getting multiple animals with different details
+     *
+     */
+    @Test
+    public void test_add_get_Animals() {
+
+    }
+
+}
