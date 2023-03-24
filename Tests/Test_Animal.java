ad54d12cb73c801618492f6e880f4688dd8ad6a3
Timothy Kim <101423302+Ssipbunhal@users.noreply.github.com>
2023-03-24 14:24:30 -0600

    Add files via upload

---
+++ Tests/Test_Animal.java
@@ -0,0 +1,66 @@
+import org.junit.Test;
+
+public class Test_Animal {
+
+    @Test
+    public void testFeedCoyote() {
+        Animal coyote = new Animal("coyote");
+        //TODO: Test feeding a coyote
+    }
+
+    @Test
+    public void testFeedFox() {
+        Animal fox = new Animal("fox");
+        //TODO: Test feeding a fox
+    }
+
+    @Test
+    public void testFeedPorcupine() {
+        Animal porcupine = new Animal("porcupine");
+        //TODO: Test feeding a porcupine
+    }
+
+    @Test
+    public void testFeedRaccoon() {
+        Animal raccoon = new Animal("raccoon");
+        //TODO: Test feeding a raccoon
+    }
+
+    @Test
+    public void testFeedBeaver() {
+        Animal beaver = new Animal("beaver");
+        //TODO: Test feeding a beaver
+    }
+
+    @Test
+    public void testCleanCage() {
+        Animal animal = new Animal("coyote");
+        //TODO: Test cleaning a cage
+    }
+
+    @Test
+    public void testFeedOrphanedAnimals() {
+        Animal animal1 = new Animal("coyote");
+        Animal animal2 = new Animal("coyote");
+        //TODO: Test feeding orphaned animals
+    }
+
+    @Test
+    public void testNocturnalFeeding() {
+        Animal animal = new Animal("fox");
+        //TODO: Test nocturnal feeding
+    }
+
+    @Test
+    public void testDiurnalFeeding() {
+        Animal animal = new Animal("beaver");
+        //TODO: Test diurnal feeding
+    }
+
+    @Test
+    public void testCrepuscularFeeding() {
+        Animal animal = new Animal("coyote");
+        //TODO: Test crepuscular feeding
+    }
+}
+
