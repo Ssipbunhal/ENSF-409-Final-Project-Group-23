499fbda1f8b14a8e7b5fb1379004ce8396f1f26b
Ssipbunhal <minggoonum2@gmail.com>
2023-04-08 13:01:28 -0600

    Adding some tests

--- Tests/Test_Tasks.java
+++ Tests/Test_Tasks.java
@@ -1,40 +1,38 @@
-
-
 import org.junit.Test;
 
 public class Test_Tasks {
 
 
     public void tasksAreInherited(){
-        
+
     }
     /*
-     * 
+     *
      */
     @Test
     public void validateMedicalTaskCorresponsWithAnimalType() {
-        
+
     }
 
     /*
-     *  orphaned kits It takes 30 minutes to feed 
+     *  orphaned kits It takes 30 minutes to feed
      * them all, and they need to be fed at the same time
      */
     @Test
     public void validateFeedKittenTimeCorrect() {
-        
+
     }
 
     /*
-    * kittens need to be fed at the same time.
-    */
+     * kittens need to be fed at the same time.
+     */
     @Test
     public void validateFeedKittenFeedAtTheSameTime() {
-        
+
     }
 
     /*
-     * For orphaned animals, feeding is considered a 
+     * For orphaned animals, feeding is considered a
      * medical task rather than a standard task
      */
     @Test
@@ -44,5 +42,7 @@ public class Test_Tasks {
 
 
 }
+
+
 
 
